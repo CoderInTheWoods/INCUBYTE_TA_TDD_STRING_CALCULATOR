a3dc24e69ed40eaee82181d1bfa9799d04dc00be
Kalgi Sheth <kalgi.tech@gmail.com>
2021-08-30 01:31:55 +0530

    TEST 2 :  Return numeric value on single number string | TDD STEP : Write Test

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -5,3 +5,7 @@ from calc.string_calculator import add
 # TEST 1: Return 0 on empty string
 def test_stringcalculator_must_return_zero_on_empty_string():
     assert add("") == 0
+
+# TEST 2: Return numeric value on single number string
+def test_stringcalculator_must_return_numeric_value_on_single_number_string():
+    assert add("2") == 2
