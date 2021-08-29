e02b02131a18bd74098f77e4f5ff1175cf5d9277
Kalgi Sheth <kalgi.tech@gmail.com>
2021-08-30 01:39:50 +0530

    TEST 3 : Return sum on two number string | TDD STEP : Write Test

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -9,3 +9,7 @@ def test_stringcalculator_must_return_zero_on_empty_string():
 # TEST 2: Return numeric value on single number string
 def test_stringcalculator_must_return_numeric_value_on_single_number_string():
     assert add("2") == 2
+
+# TEST 3: Return sum on two number string
+def test_stringcalculator_must_return_sum_on_two_number_string():
+    assert add("1,2") == 3
