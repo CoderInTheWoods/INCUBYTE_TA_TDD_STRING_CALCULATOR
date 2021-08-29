042d17d0b2bde3ade1543ea554e52411cc8a9e20
Kalgi Sheth <kalgi.tech@gmail.com>
2021-08-30 01:54:34 +0530

    CODE FOR TEST 4 | TDD STEP : Write Code + Optimize Code

--- calc/string_calculator.py
+++ calc/string_calculator.py
@@ -1,7 +1,5 @@
 def add(numbers):
     if numbers == "":
         return 0
-    elif len(numbers.split(',')) == 1:
-        return int(numbers)
-    elif len(numbers.split(',')) == 2:
-        return sum(map(int, numbers.split(',')))
+
+    return sum(map(int, numbers.split(',')))
